434b653e9835ef8f357cb3b07053150538d71c2b
KasandraD <kasandra@jane.app>
2023-06-09 08:01:08 -0700

    starting section 5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense'
 
 
 function App() {
@@ -24,10 +25,16 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log("In App.js")
+    console.log(expenses)
+
+  }
  
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
